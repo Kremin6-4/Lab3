9195f7f7e78ef8dcb7a1c8bf3885bd9940c4837e
Kremin6-4 <71972736+Kremin6-4@users.noreply.github.com>
2021-01-12 22:34:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+ /*function ready() {
+  let confirm = ("Are you ready?");
+  alert ("ready");
+}*/
+const number = document.getElementById("button")
+const num = document.getElementById()
+number.addEventListener("click", (input_id) =>  { 
+	num.input_id;
+}
+
+
+var years = prompt('What is your age?');
+if (years >= 18){
+	alert ("U are allowed to continue but I'm take no responsibility");
+	alert ("Good Luck!");
+}else{
+	alert ("Sorry, the content is available for viewing since the age of 18!");
+}
+
+function sqrt(){
+var b = document.getElementById("input_id").value;
+alert((Math.sqrt(b)));
+}
+	 
